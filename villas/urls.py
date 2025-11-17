25a7d894d3b9505c1d435cdfbd82245c77e330d2
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-11-17 07:05:51 +0600

    Remove unused viewsets from URL routing and register PropertyViewSet for properties

--- villas/urls.py
+++ villas/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import VillaViewSet, BookingViewSet, AmenityViewSet, VillaImageViewSet
+from .views import PropertyViewSet 
+
 
 router = DefaultRouter()
-router.register('villas', VillaViewSet, basename='villa')
-router.register('bookings', BookingViewSet, basename='booking')
-router.register('amenities', AmenityViewSet, basename='amenity')
-router.register('villa-images', VillaImageViewSet, basename='villaimage')
+router.register(r'properties', PropertyViewSet, basename='property')
 
 urlpatterns = [
     path('', include(router.urls)),
