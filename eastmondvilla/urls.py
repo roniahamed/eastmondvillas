d421f988706f648a2bec19387aaa3439c0fdf3a7
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-11-19 04:09:15 +0600

    Uncomment villas URL inclusion in urlpatterns and add __init__.py for management commands in villas app

--- eastmondvilla/urls.py
+++ eastmondvilla/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
-    # path('api/villas/', include('villas.urls')),
+    path('api/villas/', include('villas.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
