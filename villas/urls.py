6c3304f7250e075521413162fbc3502cb4614302
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-11-18 07:07:43 +0600

    Add booking routes to the URL configuration

--- villas/urls.py
+++ villas/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PropertyViewSet 
+from .views import PropertyViewSet, BookingViewSet, get_property_availability
 
 
 router = DefaultRouter()
 router.register(r'properties', PropertyViewSet, basename='property')
+router.register(r'bookings', BookingViewSet, basename='booking')
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('properties/<int:property_pk>/availability/', get_property_availability, name='property-availability'
+),
 ]
