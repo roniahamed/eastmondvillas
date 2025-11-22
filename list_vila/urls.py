9a1f3f900bb5ed70bccf50ebfe767352e5395c6b
tntanvir <tntanvir2382018@gmail.com>
2025-11-23 03:10:04 +0600

    notifications setup and push

--- list_vila/urls.py
+++ list_vila/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path
-from .views import vila_list,ContectUsView
+from .views import vila_list,ContactUsView
 urlpatterns = [
     path('list/', vila_list.as_view(), name='list_vila_home'),
     path('list/<int:pk>/', vila_list.as_view(), name='list_vila_home'),
-    path('contect/', ContectUsView.as_view(), name='contect_us'),
-    path('contect/<int:pk>/', ContectUsView.as_view(), name='contect_us'),
+    path('contect/', ContactUsView.as_view(), name='contect_us'),
+    path('contect/<int:pk>/', ContactUsView.as_view(), name='contect_us'),
 ]  
