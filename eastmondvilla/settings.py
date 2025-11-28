418c0c7ae3e60f57841edae34d346a33edc03039
roniahamed <mdroniahamed56@gmail.com>
2025-11-29 05:30:04 +0600

    feat: Extend JWT token lifetimes to enhance session duration

--- eastmondvilla/settings.py
+++ eastmondvilla/settings.py
@@ -205,8 +205,8 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 224288000
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=100),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=200),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
