71a47e69b74a89e822d5d248fdfd8997b19d7c26
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-04 02:05:11 +0600

    feat: Add ResourceMediaSerializer and update ResourceSerializer to handle uploaded files

--- resources/serializers.py
+++ resources/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from .models import Resource
+from .models import Resource, ResourceMedia
+
+
+class ResourceMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResourceMedia
+        fields = ['id', 'file']
 
 class ResourceSerializer(serializers.ModelSerializer):
+    files = ResourceMediaSerializer(source="media_resources", many=True, read_only=True)
+    uploaded_files = serializers.ListField(
+        child=serializers.FileField(), write_only=True, required=False
+    )
     class Meta:
         model = Resource
-        fields = '__all__'
+        fields = ['id', 'title', 'category', 'description', 'files', 'updated_at', 'created_at', 'uploaded_files']
+        read_only_fields = ['updated_at', 'created_at', 'files']
+    
+    def create(self, validated_data):
+        uploaded_files = validated_data.pop('uploaded_files', [])
+        resource = Resource.objects.create(**validated_data)
+        for file in uploaded_files:
+            ResourceMedia.objects.create(resource=resource, file=file)
+        return resource
