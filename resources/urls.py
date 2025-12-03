3cea4d30448d88dc7f8be1b9aefe146bc6ade478
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-04 05:16:47 +0600

    feat: Update URL routing to use ResourceViewSet for resource management

--- resources/urls.py
+++ resources/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+from rest_framework.routers import DefaultRouter
+
+routers = DefaultRouter()
+routers.register(r'', ResourceViewSet, basename='resource')
 
 urlpatterns = [
-    path("", ResourceListAPIView.as_view(), name="resource-list"),
+    path('', include(routers.urls)),
 ]
