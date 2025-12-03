b62d5da1becb5c782f2651f1fadf9d8d3834950c
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-04 03:12:23 +0600

    feat: Implement FAQViewSet for managing FAQs with filtering and search capabilities

--- faqs/views.py
+++ faqs/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from faqs.models import FAQ
+from faqs.serializers import FAQSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.permissions import IsAgentOrAdminOrManager
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter 
 
-# Create your views here.
+
+class FAQViewSet(viewsets.ModelViewSet):
+    queryset = FAQ.objects.all()
+    serializer_class = FAQSerializer
+    permission_classes = [IsAgentOrAdminOrManager]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['category']
+    search_fields = ['question', 'answer', 'category']
+    ordering_fields = ['category', 'question']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
