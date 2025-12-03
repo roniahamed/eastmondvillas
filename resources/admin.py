47b1db5dbf064d16aa265f1a50961e7afe665c0a
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-04 02:05:03 +0600

    feat: Enhance admin interface by adding ResourceMedia inline and updating ResourceAdmin configuration

--- resources/admin.py
+++ resources/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin, TabularInline 
 
 # Register your models here.
-from .models import Resource
+from .models import Resource, ResourceMedia
+
+
+class ResourceMediaInline(TabularInline):
+    model = ResourceMedia
+    extra = 1
+
+
 admin.site.register(Resource)
+class ResourceAdmin(ModelAdmin):
+    list_display = ('title', 'category', 'created_at', 'updated_at')
+    search_fields = ('title', 'description', 'category')
+    list_filter = ('category', 'created_at', 'updated_at')
+    inlines = [ResourceMediaInline]
+
+
+admin.site.register(ResourceMedia)
+
+
+class ResourceMediaAdmin(ModelAdmin):
+    list_display = ('resource', 'file')
