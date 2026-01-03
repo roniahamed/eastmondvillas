5dccef3030ae8ceed043e4339d93c7a4c662d2f4
roniahamed <mdroniahamed56@gmail.com>
2026-01-04 01:55:31 +0600

    feat: Refactor send_email function to include error handling for SMTP authentication

--- list_vila/utils.py
+++ list_vila/utils.py
@@ -1,19 +1,22 @@
 import requests
 from django.conf import settings
 from django.core.mail import send_mail
+from smtplib import SMTPAuthenticationError
 
 
 def send_email(subject, recipient_list, message=None, template=None):
 
     if template:
         message = template
-
-    send_mail(
-        subject,
-        message,
-        settings.DEFAULT_FROM_EMAIL,
-        html_message=template,
-        recipient_list=recipient_list,
-        fail_silently=False,
-    )
+    try:
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            html_message=template,
+            recipient_list=recipient_list,
+            fail_silently=False,
+        )
+    except SMTPAuthenticationError as e:
+        pass 
     
